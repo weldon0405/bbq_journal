8f0b70fc1429e4a06fb793810b2daa9393819cac
Weldon <weldon0405@gmail.com>
2017-12-20 00:10:08 -0500

    Tried printing object to terminal after creating with script. UNSUCCESSFUL *******

--- bbq_journal.py
+++ bbq_journal.py
@@ -7,7 +7,17 @@ class BbqJournal:
         self.recipe = recipe
         self.weather = weather
         self.day = datetime.date.today()
+    def __str__(self):
+        sb = []
+        for key in self.__dict__:
+            sb.append("{key}='{value}'".format(key=key, value=self.__dict__[key]))
+ 
+        return ', '.join(sb)
+ 
+    def __repr__(self):
+        return self.__str__()
 
+# new_entry = None
 
 def create_journal():
     title = input("Please enter a title for this journal entry: ")
@@ -16,12 +26,15 @@ def create_journal():
     recipe = input("Enter your recipe: ")
     # Retrieve the weather data from somewhere
     weather = input("Enter weather notes: ")
-    
+    new_entry = BbqJournal(title, meat, recipe, weather)
+    print(new_entry)
+    return new_entry
+
 
 
 print("\n\n######################### BBQ JOURNAL #########################\n\n"
             "Welcome to the BBQ Journal. This is your place to document your\n"
-            "current cooks in pursuit of that perfect smoked flavor.\n"
+            "current cooks in pursuit of that perfect smoked meat flavor.\n"
         )
 
 user_start = input("\n\nTo create a new journal entry type NEW, otherwise press the ENTER\n"
